e5abb3c64f54a6d297df6cb76c4ad403cb3cec51
Ivan Martinez <ivan.martinez@sourcemeridian.com>
2022-10-21 10:23:39 -0500

    deploy success netlify

--- src/app/services/dogs-breeds.service.ts
+++ src/app/services/dogs-breeds.service.ts
@@ -1,26 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable()
 export class DogsBreedsService {
 
-  service_url = 'https://dog.ceo/api/breeds/list/all'
+  // service_url = 'https://dog.ceo'
   
   constructor( private http: HttpClient){
     console.log('*** Dog Breeds Service Running ***')
   }
 
   getDogBreeds(): Observable<any> {
-    return this.http.get<any>('/api/breeds/list/all')
+    return this.http.get<any>(`${environment.service_url}/api/breeds/list/all`)
   }
 
   getImages(breed: any,subBreed: any): Observable<any> {
-    return subBreed ? this.http.get<any>(`/api/breed/${breed}/${subBreed}/images/random/4`) : this.http.get<any>(`/api/breed/${breed}/images/random/4`)
+    return subBreed ? this.http.get<any>(
+      `${environment.service_url}/api/breed/${breed}/${subBreed}/images/random/4`) :
+       this.http.get<any>(`${environment.service_url}/api/breed/${breed}/images/random/4`)
   }
 
   getDogBreeds_by_name(name: string): Observable<any> {
-    return this.http.get<any>('/api/breeds/list/all')
+    return this.http.get<any>(`${environment.service_url}/api/breeds/list/all`)
   }
   
 }
